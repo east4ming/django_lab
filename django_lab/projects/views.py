4c75f778c7a9b258c3ce81673afa03795340bfce
east4ming <cuikaidong@foxmail.com>
2023-01-09 16:58:10 +0800

    ✨创建 template

--- django_lab/projects/views.py
+++ django_lab/projects/views.py
@@ -1,8 +1,6 @@
-from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 # Create your views here.
 def project_list(request):
-    return HttpResponse("<h1>Hello everyone!</h1>")
+    return render(request, "projects/index.html")
