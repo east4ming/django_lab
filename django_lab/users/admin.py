a58651d0eb35a9b9dd6b4a0e80600cdd0d8eaf0b
east4ming <cuikaidong@foxmail.com>
2023-01-09 19:18:34 +0800

    功能 register Project to admin

--- django_lab/users/admin.py
+++ django_lab/users/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
+from django_lab.projects.models import Project
 from django_lab.users.forms import UserAdminChangeForm, UserAdminCreationForm
 
 User = get_user_model()
@@ -32,3 +33,6 @@ class UserAdmin(auth_admin.UserAdmin):
     )
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
+
+
+admin.site.register(Project)
