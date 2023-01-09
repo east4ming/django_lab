65052548e34f03380f8b189f37a0ce2327498ff7
east4ming <cuikaidong@foxmail.com>
2023-01-09 19:10:15 +0800

    ✨projects app finished

--- django_lab/projects/urls.py
+++ django_lab/projects/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from django_lab.projects import views
 
+app_name = "projects"
 urlpatterns = [
-    path("", view=views.project_list),
+    path("test", view=views.test),
+    path("", view=views.all_projects, name="all_projects"),
+    path("<int:pk>", view=views.project_detail, name="project_detail"),
 ]
--- django_lab/projects/views.py
+++ django_lab/projects/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
+from django_lab.projects.models import Project
+
 
 # Create your views here.
-def project_list(request):
+def test(request):
     return render(request, "projects/index.html")
+
+
+def all_projects(request):
+    """Query the db to return all project objects."""
+    projects = Project.objects.all()
+    return render(request, "projects/all_projects.html", {"projects": projects})
+
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, "projects/detail.html", {"project": project})
