5622696d9f98163e838e48c46cb98124b75de8ff
east4ming <cuikaidong@foxmail.com>
2023-01-10 17:06:01 +0800

    🔧local.py增加tailscale域名

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="X2ec5MPZdWZxZoiu6kCxlz9fwzEARiK2OfxGpoVS4wNn3itJH5PXHXQO3ojBOATI",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", ".west-beta.ts.net"]
 
 # CACHES
 # ------------------------------------------------------------------------------
