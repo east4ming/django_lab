69f51969012524c0c43d367ef70e3f13f298503d
east4ming <cuikaidong@foxmail.com>
2023-01-17 16:25:13 +0800

    🐛bug fixed: add gis engine

--- config/settings/base.py
+++ config/settings/base.py
@@ -40,6 +40,8 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {"default": env.db("DATABASE_URL")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
+# https://realpython.com/location-based-app-with-geodjango-tutorial/#configuring-the-postgresql-database
+DATABASES["default"]["ENGINE"] = "django.contrib.gis.db.backends.postgis"
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
