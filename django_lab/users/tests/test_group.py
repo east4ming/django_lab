474f8cd7234902cfc817c1d3795186174ebcbbe8
east4ming <cuikaidong@foxmail.com>
2023-01-18 15:56:24 +0800

    ✅factory fixture

--- django_lab/users/tests/test_group.py
+++ django_lab/users/tests/test_group.py
@@ -3,21 +3,6 @@ from django.contrib.auth.models import Group, Permission
 
 from django_lab.users.models import User
 
-# from django.test import TestCase
-
-
-# class MyTest(TestCase):
-#     fixtures = ["group.json"]
-
-#     def test_should_create_group(self):
-#         group = Group.objects.get(pk=1)
-#         self.assertEqual(group.name, "appusers")
-
-
-# def test_should_create_user_with_username(db) -> None:
-#     user = User.objects.create_user("Haki")
-#     assert user.username == "Haki"
-
 
 @pytest.fixture
 def app_user_group(db) -> Group:
@@ -30,25 +15,52 @@ def app_user_group(db) -> Group:
 
 
 @pytest.fixture
-def user_A(db, app_user_group: Group) -> User:
-    user = User.objects.create_user("A")
-    user.groups.add(app_user_group)
-    return user
+def app_user_factory(db, app_user_group: Group):
+    # Closure
+    def create_app_user(
+        username: str,
+        password: str | None = None,
+        email: str | None = "foo@bar.com",
+        is_staff: bool = False,
+        is_superuser: bool = False,
+        is_active: bool = True,
+        groups: list[Group] = [],
+    ) -> User:
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email,
+            is_staff=is_staff,
+            is_superuser=is_superuser,
+            is_active=is_active,
+        )
+        user.groups.add(app_user_group)
+        # Add additional groups, if provided
+        user.groups.add(*groups)
+        return user
+
+    return create_app_user
 
 
 @pytest.fixture
-def user_B(db, app_user_group: Group) -> User:
-    user = User.objects.create_user("B")
-    user.groups.add(app_user_group)
-    return user
+def user_A(db, app_user_factory) -> User:
+    return app_user_factory("A")
 
 
-def test_should_create_user(user_A: User) -> None:
+@pytest.fixture
+def user_B(db, app_user_factory) -> User:
+    return app_user_factory("B")
+
+
+def test_should_create_user(user_A: User, app_user_group: Group) -> None:
     assert user_A.username == "A"
+    assert user_A.email == "foo@bar.com"
 
 
-def test_user_is_in_app_user_group(user_A: User) -> None:
-    assert user_A.groups.filter(name="app_user").exists()
+def test_should_create_user_in_app_user_group(
+    user_A: User, app_user_group: Group
+) -> None:
+    assert user_A.groups.filter(pk=app_user_group.pk).exists()
 
 
 def test_should_check_password(db, user_A: User) -> None:
@@ -62,5 +74,5 @@ def test_should_not_check_unusable_password(db, user_A: User) -> None:
     assert user_A.check_password("secret") is False
 
 
-def test_should_create_two_user(user_A: User, user_B: User) -> None:
+def test_should_create_two_users(user_A: User, user_B: User) -> None:
     assert user_A.pk != user_B.pk
