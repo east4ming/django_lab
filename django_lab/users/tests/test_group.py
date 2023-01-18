bc18490dc938075ba33b39d04cf4253d0dcc634a
east4ming <cuikaidong@foxmail.com>
2023-01-18 11:53:23 +0800

    ✅需求变化时维护夹具

--- django_lab/users/tests/test_group.py
+++ django_lab/users/tests/test_group.py
@@ -1,26 +1,42 @@
 import pytest
-from django.contrib.auth.models import Group
-from django.test import TestCase
+from django.contrib.auth.models import Group, Permission
 
 from django_lab.users.models import User
 
-
-class MyTest(TestCase):
-    fixtures = ["group.json"]
-
-    def test_should_create_group(self):
-        group = Group.objects.get(pk=1)
-        self.assertEqual(group.name, "appusers")
+# from django.test import TestCase
 
 
-def test_should_create_user_with_username(db) -> None:
-    user = User.objects.create_user("Haki")
-    assert user.username == "Haki"
+# class MyTest(TestCase):
+#     fixtures = ["group.json"]
+
+#     def test_should_create_group(self):
+#         group = Group.objects.get(pk=1)
+#         self.assertEqual(group.name, "appusers")
+
+
+# def test_should_create_user_with_username(db) -> None:
+#     user = User.objects.create_user("Haki")
+#     assert user.username == "Haki"
 
 
 @pytest.fixture
 def user_A(db) -> User:
-    return User.objects.create_user("A")
+    group = Group.objects.create(name="app_user")
+    change_user_permissions = Permission.objects.filter(
+        codename=["change_user", "view_user"],
+    )
+    group.permissions.add(*change_user_permissions)
+    user = User.objects.create_user("A")
+    user.groups.add(group)
+    return user
+
+
+def test_should_create_user(user_A: User) -> None:
+    assert user_A.username == "A"
+
+
+def test_user_is_in_app_user_group(user_A: User) -> None:
+    assert user_A.groups.filter(name="app_user").exists()
 
 
 def test_should_check_password(db, user_A: User) -> None:
