63bc8a4576a08c139c9550c624ca2be39e5d13a8
east4ming <cuikaidong@foxmail.com>
2023-01-18 11:32:42 +0800

    ✅pytest-django 学习使用

--- django_lab/users/tests/test_group.py
+++ django_lab/users/tests/test_group.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import Group
 from django.test import TestCase
 
+from django_lab.users.models import User
+
 
 class MyTest(TestCase):
     fixtures = ["group.json"]
@@ -8,3 +10,8 @@ class MyTest(TestCase):
     def test_should_create_group(self):
         group = Group.objects.get(pk=1)
         self.assertEqual(group.name, "appusers")
+
+
+def test_should_create_user_with_username(db) -> None:
+    user = User.objects.create_user("Haki")
+    assert user.username == "Haki"
