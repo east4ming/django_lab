dbe6e8846023fda17400f685f76a410b836d1593
east4ming <cuikaidong@foxmail.com>
2023-01-18 11:41:23 +0800

    ✅pytest fixture 学习

--- django_lab/users/tests/test_group.py
+++ django_lab/users/tests/test_group.py
@@ -1,3 +1,4 @@
+import pytest
 from django.contrib.auth.models import Group
 from django.test import TestCase
 
@@ -15,3 +16,19 @@ class MyTest(TestCase):
 def test_should_create_user_with_username(db) -> None:
     user = User.objects.create_user("Haki")
     assert user.username == "Haki"
+
+
+@pytest.fixture
+def user_A(db) -> User:
+    return User.objects.create_user("A")
+
+
+def test_should_check_password(db, user_A: User) -> None:
+    user_A.set_password("secret")
+    assert user_A.check_password("secret") is True
+
+
+def test_should_not_check_unusable_password(db, user_A: User) -> None:
+    user_A.set_password("secret")
+    user_A.set_unusable_password()
+    assert user_A.check_password("secret") is False
